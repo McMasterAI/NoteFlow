9d8aed636de0d07a8afc4d9370dc46651e489b56
MFactor <mingsqu@gmail.com>
2023-11-30 17:16:02 -0500

    Updated simplifyScentences.py

--- src/simplifyScentences.py
+++ src/simplifyScentences.py
@@ -2,11 +2,22 @@ import spacy
 from spacy import displacy
 
 nlp = spacy.load("en_core_web_trf")
-doc = nlp("This is a particularly tricky idea, Python only has Non-static variable. Static variables belong to the class, and is inherited by each object (This is good for things that are ubiquitous within all objects of a class). Static variables can still be used like non-static, to display specific information about a object, but the problem arises if you try and call them through the class, when there is no specific information available. This is where non-static comes in, since non-static cannot be called through the class, this problem goes away. Non-static is good for information that is specific to each class (non-ubiquitous)")
+doc = nlp("The EU is the biggest non-miliary entity in the world!")
+#displacy.serve(doc, style="dep")
 
+output_list = []
+del_num = 0
 
-for token in doc:
-    #print(token.text, token.lemma_, token.pos_, token.tag_, token.dep_,
-    #        token.shape_, token.is_alpha, token.is_stop)
-    if (token.pos_ != "AUX"):
-        print(token.text, end=" ")
+for index, token in enumerate(doc):
+    if (token.pos_ == "CCONJ") or (token.pos_ == "AUX") or (token.pos_ == "PRON") or (token.pos_ == "PROPN") or (token.text == "The") or (token.text == "the"):
+        print("Removed: ", token.text)
+        del_num += 1
+    else:
+        break
+
+for index, token in enumerate(doc):
+    if (del_num <= index):
+        output_list.append(token.text)
+
+for item in output_list:
+    print(item, end=' ')
