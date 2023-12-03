75309f9b75293b4fd42ea044208712347f725a03
MFactor <mingsqu@gmail.com>
2023-12-03 16:31:08 -0500

    Added title extraction

--- src/simplifyScentences.py
+++ src/simplifyScentences.py
@@ -2,16 +2,20 @@ import spacy
 from spacy import displacy
 
 nlp = spacy.load("en_core_web_trf")
-doc = nlp("The EU is the biggest non-miliary entity in the world!")
+doc = nlp("Thus, Apple is planning on building a new office in Toronto for 13 million U.S. Dollars.")
 #displacy.serve(doc, style="dep")
 
 output_list = []
 del_num = 0
 
 for index, token in enumerate(doc):
-    if (token.pos_ == "CCONJ") or (token.pos_ == "AUX") or (token.pos_ == "PRON") or (token.pos_ == "PROPN") or (token.text == "The") or (token.text == "the"):
+    if (token.pos_ == "CCONJ") or (token.pos_ == "AUX") or (token.pos_ == "PRON") or (token.pos_ == "PROPN") or (token.pos_ == "ADV") or (token.text == "The") or (token.text == "the"):
         print("Removed: ", token.text)
         del_num += 1
+    elif (token.text == ","):
+        print("Removed: ", token.text)
+        del_num += 1
+        break
     else:
         break
 
