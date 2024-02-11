60e8080d283e8ca338c6b3a4cb6279597505a400
leiah8 <leiah.nay8@gmail.com>
2024-02-10 21:35:55 -0500

    adjust number of points to length

--- src/t5_sentence_extract.py
+++ src/t5_sentence_extract.py
@@ -3,9 +3,10 @@ import spacy
 
 nlp = spacy.load("en_core_web_sm")
 
-def generate_summary(text, model, tokenizer):
-    inputs = tokenizer.encode("summarize: " + text, return_tensors="pt", max_length=8192, truncation=True)
-    summary_ids = model.generate(inputs, max_length=1024, length_penalty=2.0, num_beams=4, early_stopping=True)
+def generate_summary(text, model, tokenizer, l):
+    m = max(l, 1892)
+    inputs = tokenizer.encode("summarize: " + text, return_tensors="pt", max_length=m, truncation=True)
+    summary_ids = model.generate(inputs, max_length=(m/4), length_penalty=2.0, num_beams=4, early_stopping=True)
 
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary
@@ -25,9 +26,12 @@ def main(input_text):
     #input_text = "Coined by Karl Marx, the bourgeoisie represents a social class characterized by its ownership of capital, such as factories and businesses, and its ability to accumulate wealth through the exploitation of labor. In the sociological framework, the bourgeoisie holds a position of power and influence, shaping economic structures and policies to maintain its interests. The concept emphasizes the dichotomy between the bourgeoisie and the proletariat, the working class, highlighting the inherent class struggle within capitalist societies. From a sociological perspective, the bourgeoisie not only signifies economic dominance but also wields cultural and political influence, contributing to the perpetuation of social inequalities and class-based divisions. This lens allows sociologists to analyze the dynamics of power, class conflict, and social change within the broader context of capitalist societies."
 
     # Generate summary
-    summary = generate_summary(input_text, model, tokenizer)
+    summary = generate_summary(input_text, model, tokenizer, len(input_text))
 
     # summary to bullet points
     bullet_points = to_bullet_points(summary)
 
     return (bullet_points)
+
+
+#print(main("Coined by Karl Marx, the bourgeoisie represents a social class characterized by its ownership of capital, such as factories and businesses, and its ability to accumulate wealth through the exploitation of labor. In the sociological framework, the bourgeoisie holds a position of power and influence, shaping economic structures and policies to maintain its interests. The concept emphasizes the dichotomy between the bourgeoisie and the proletariat, the working class, highlighting the inherent class struggle within capitalist societies. From a sociological perspective, the bourgeoisie not only signifies economic dominance but also wields cultural and political influence, contributing to the perpetuation of social inequalities and class-based divisions. This lens allows sociologists to analyze the dynamics of power, class conflict, and social change within the broader context of capitalist societies."))
