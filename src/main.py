3a39e39e172ae762c4960f2d4d2393e5eb7b464b
MFactor1 <mingsqu@gmail.com>
2024-02-29 22:19:59 -0500

    Final setup for CUCAI

--- src/main.py
+++ src/main.py
@@ -1,11 +1,28 @@
-import t5_sentence_extract as summarize
+import t5_sentence_extract as sentenceGen
 import titleUsingTextRank as titleGen
 import boldingItalicizationModule as bi
+import gradio as gr
 
+def summarize(notes):
+    bullet_points = sentenceGen.main(notes)
+    paragraphed_points = " ".join(bullet_points).replace(" -",".").replace("- ","")
+    title = titleGen.generateTitle(notes)
+    bullet_points = bi.boldItalicize(paragraphed_points, bullet_points)
+    output = title + "\n" + "\n".join(bullet_points)
+    return output
 
-input_text = input("Enter text to summarize: ")
+demo = gr.Interface(fn=summarize,
+                    inputs=["textbox"],
+                    outputs=gr.Markdown()
+)
 
-bullet_points = summarize.main(input_text)
+demo.launch(share=True)
+
+#Backup terminal based main
+"""
+input_text = input("Text to input: ")
+
+bullet_points = sentenceGen.main(input_text)
 paragraphed_points = " ".join(bullet_points).replace(" -",".").replace("- ","")
 title = titleGen.generateTitle(input_text)
 bullet_points = bi.boldItalicize(paragraphed_points, bullet_points)
@@ -13,3 +30,4 @@ bullet_points = bi.boldItalicize(paragraphed_points, bullet_points)
 print(title)
 print("\n".join(bullet_points))
 print()
+"""
